56c4654fd9b219b5fe8964838c30b20296e09b6f
nicodemos <nicodemossjunior@gmail.com>
2013-08-07 01:27:12 -0300

    Gravação de jogador

--- JampaXadrez/src/br/com/praia/jampaxadrez/MenuActivity.java
+++ JampaXadrez/src/br/com/praia/jampaxadrez/MenuActivity.java
@@ -14,7 +14,7 @@ import com.db4o.ObjectContainer;
 
 public class MenuActivity extends Activity implements OnClickListener{
 
-	private ObjectContainer db;
+	public static ObjectContainer db;
 	private static final String LOG_NAME = "jampaXadrez";
 
 	@Override
@@ -26,16 +26,24 @@ public class MenuActivity extends Activity implements OnClickListener{
 		((ImageButton)findViewById(R.id.imRank)).setOnClickListener(this);
 		((ImageButton)findViewById(R.id.imAbout)).setOnClickListener(this);
 		
-		String DB_NAME = "jampaXadrez.db4o";
-		String DB_PATH = getDir("", MODE_PRIVATE).getPath();
+		configureDB();
 		
-		try {
-			db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),DB_PATH + "/" + DB_NAME);
-		} catch (Exception e) {
-			Log.e(LOG_NAME, "Nao foi possivel abrir o arquivo : " + DB_PATH + "/" + DB_NAME 
-					+"\n Mensagem original: " + e.getMessage());
+	}
+
+	public void configureDB() {
+		if (db == null || db.ext().isClosed()) {
+			String DB_NAME = "jampaXadrez.db4o";
+			String DB_PATH = getDir("", MODE_PRIVATE).getPath();
+			try {
+				db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),
+						DB_PATH + "/" + DB_NAME);
+			} catch (Exception e) {
+				Log.e(LOG_NAME,
+						"Nao foi possivel abrir o arquivo : " + DB_PATH + "/"
+								+ DB_NAME + "\n Mensagem original: "
+								+ e.getMessage());
+			}
 		}
-		
 	}
 
 	@Override
--- JampaXadrez/src/br/com/praia/jampaxadrez/PlayActivity.java
+++ JampaXadrez/src/br/com/praia/jampaxadrez/PlayActivity.java
@@ -1,16 +1,31 @@
 package br.com.praia.jampaxadrez;
 
+import br.com.praia.jampaxadrez.model.Jogador;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class PlayActivity extends Activity {
+public class PlayActivity extends Activity implements OnClickListener {
 
+	private static final String LOG_NAME = "jampaXadrez";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
+		
+		findViewById(R.id.bt_jogador1).setOnClickListener(this);
+		findViewById(R.id.bt_jogador2).setOnClickListener(this);
 		
 		populate();
 	}
@@ -28,6 +43,47 @@ public class PlayActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.play, menu);
 		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.bt_jogador1:
+			cadastrarJogador((EditText)findViewById(R.id.ed_tx_jogador1));
+			break;
+
+		case R.id.bt_jogador2:
+			cadastrarJogador((EditText)findViewById(R.id.ed_tx_jogador2));
+			break;
+		}
+		
+	}
+
+	/**
+	 * @param findViewById
+	 */
+	private void cadastrarJogador(EditText nomeJogador) {
+		Jogador jogador = new Jogador();
+		jogador.setName(nomeJogador.getText().toString());
+		
+		ObjectContainer db = MenuActivity.db;
+		if (db != null && !db.ext().isClosed()) {
+			ObjectSet<Jogador> lista = db.queryByExample(jogador);
+			if (!lista.isEmpty()) {
+				Log.i(LOG_NAME, "Jogador " + jogador.getName() + " encontrado");
+			} else {
+				db.store(jogador);
+				db.commit();
+				Toast.makeText(this,
+						"Jogador " + jogador.getName() + " cadastrado.",
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+		
+		
 	}
 
 }
