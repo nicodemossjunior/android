6790529005660f465558c674eda2104262414c8d
nicodemos <nicodemossjunior@gmail.com>
2013-08-09 01:24:56 -0300

    Ranking de jogadores

--- JampaXadrez/src/br/com/praia/jampaxadrez/RankingActivity.java
+++ JampaXadrez/src/br/com/praia/jampaxadrez/RankingActivity.java
@@ -1,8 +1,20 @@
 package br.com.praia.jampaxadrez;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import br.com.praia.jampaxadrez.model.Jogador;
+
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
 
 public class RankingActivity extends Activity {
 
@@ -10,6 +22,35 @@ public class RankingActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ranking);
+		
+		populate();
+	}
+
+	private void populate() {
+		ObjectContainer db = MenuActivity.db;
+		if (db != null && !db.ext().isClosed()) {
+			Query q = db.query();
+			q.constrain(Jogador.class);
+			ObjectSet<Jogador> jogadores = q.execute();
+			List<Jogador> arr = new ArrayList<Jogador>();
+			
+			if (jogadores != null && !jogadores.isEmpty()) {
+				do {
+					arr.add(jogadores.next());
+				} while (jogadores.hasNext());
+				
+				Collections.sort(arr, new Comparator<Jogador>() {
+					@Override
+					public int compare(Jogador j1, Jogador j2) {
+						return j1.getVitorias() < j2.getVitorias() ? 1 : j1.getVitorias() > j2.getVitorias() ? -1 : 0;
+					}
+				});
+				
+				ArrayAdapter<Jogador> adapter = new ArrayAdapter<Jogador>(this, android.R.layout.simple_list_item_1, arr);
+				ListView listView = (ListView) findViewById(R.id.lv_ranking);
+				listView.setAdapter(adapter);
+			}
+		}
 	}
 
 	@Override
